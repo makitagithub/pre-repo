4553f64a443265097a52b00e5d997e8a1e351e7f
makitagithub <yukizuki0204@yahoo.co.jp>
2024-08-26 21:30:50 +0900

    all in

---
+++ MulTen.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class MulTen {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = 10;
+        int c = a * b;
+        System.out.println(c);
+    }
+}
